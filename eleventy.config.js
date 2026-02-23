9828fe55130e142b64516206b4b1a8d8b9d24018
ug-gds <umma.gohil@digital.cabinet-office.gov.uk>
2026-02-23 15:17:25 +0000

    feat(dfc-1121): navigation in

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,13 +4,40 @@ import { govukEleventyPlugin } from "@x-govuk/govuk-eleventy-plugin";
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     serviceName: "Design System",
+    homeKey: "Home",
+    header: {
+      productName: "Design Hub",
+      search: { indexPath: "/search.json" },
+    },
+    serviceNavigation: {
+      serviceUrl: "/",
+      search: {
+        indexPath: "/search-index.json",
+        sitemapPath: "/sitemap",
+      },
+      navigation: [
+        {
+          text: "Get started",
+          href: "/get-started/",
+        },
+        {
+          text: "Features",
+          href: "/features/",
+        },
+        {
+          text: "Layouts",
+          href: "/layouts/",
+        },
+      ],
+    },
   });
 
   return {
     dir: {
-      input: "src",
+      input: "src/index.njk",
       output: "docs",
-      layouts: "./_includes/base.njk",
+      layouts: "_layouts",
+      includes: "../node_modules/@x-govuk/govuk-eleventy-plugin/layouts",
     },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
